cef7dd80040a7a06f1016e8998873c127b8296a3
Daniel Nascimento <me@ondaniel.com.br>
2024-04-11 18:46:18 -0300

    feat(tasks): add tasks and psql to app module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 
-import { ClassSerializerInterceptor } from 'common/interceptors/class-serializer.interceptor';
+import { TaskModule } from 'modules/tasks/task.module';
 
 import { CacheModule } from 'providers/cache/cache.module';
 import { HashModule } from 'providers/hash/hash.module';
-import { MongodbModule } from 'providers/mongodb/mongodb.module';
+import { PostgresqlModule } from 'providers/postgresql/postgresql.module';
 
 import { AuthModule } from '../auth/auth.module';
 import { RateLimitingGuard } from '../rate-limiting/guards/rate-limiting.guard';
@@ -17,8 +17,9 @@ import { UserModule } from '../user/user.module';
     AuthModule,
     CacheModule,
     HashModule,
-    MongodbModule,
+    PostgresqlModule,
     RateLimitingModule,
+    TaskModule,
     UserModule,
   ],
   providers: [
