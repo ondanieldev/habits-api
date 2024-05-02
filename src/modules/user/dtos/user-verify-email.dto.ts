756b9d6cd50c29854aed2346f68bb63717c84afc
Daniel Nascimento <me@ondaniel.com.br>
2024-05-02 20:31:28 -0300

    refactor(verify-email): delete previous strategy

--- src/modules/user/dtos/user-verify-email.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString } from 'class-validator';
-
-import { UserVerifyEmailBo } from '../bos/user-verify-email.bo';
-
-export class UserVerifyEmailDto implements UserVerifyEmailBo {
-  @IsString()
-  verifyEmailToken: string;
-}
